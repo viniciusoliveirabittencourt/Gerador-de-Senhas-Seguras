faeb57760956d553ab84f1362c1ea5dffc536b35
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2023-07-02 17:26:10 -0300

    adicionando um estilo bonitinho para a publicação

--- src/main.js
+++ src/main.js
@@ -4,9 +4,16 @@ import "./style.css"
 
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEl = document.querySelector('h2');
+const copyTranferenceArea = document.querySelector('h3');
+
+const removeClassNone = () => {
+  copyTranferenceArea.classList.add("none")
+}
 
 passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
   copy(randomPassword);
+  copyTranferenceArea.classList.remove("none");
+  setTimeout(removeClassNone, 3000);
 });
